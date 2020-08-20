bb6b1189637c7571984332fda50e58b418de5855
a-poor <a-poor@users.noreply.github.com>
2020-08-20 13:34:18 -0400

    Temporarily removing data tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,31 +1,31 @@
 
-from apoor import data
+# from apoor import data
 
 
-def test_list_data():
-    d = data.list_data()
-    assert "iris" in d
-    assert "boston" in d
+# def test_list_data():
+#     d = data.list_data()
+#     assert "iris" in d
+#     assert "boston" in d
 
-def test_load_iris():
-    df = data.load_iris()
-    iris_shape = (150, 5)
-    assert df.shape == iris_shape
-    iris_cols = [
-        'sepal_length','sepal_width', 
-        'petal_length','petal_width', 
-        'target']
-    assert df.columns.to_list() == iris_cols
+# def test_load_iris():
+#     df = data.load_iris()
+#     iris_shape = (150, 5)
+#     assert df.shape == iris_shape
+#     iris_cols = [
+#         'sepal_length','sepal_width', 
+#         'petal_length','petal_width', 
+#         'target']
+#     assert df.columns.to_list() == iris_cols
     
-def test_load_boston():
-    df = data.load_boston()
-    boston_shape = (506, 14)
-    assert df.shape == boston_shape
-    boston_cols = [
-        'CRIM','ZN','INDUS',
-        'CHAS','NOX','RM',
-        'AGE','DIS','RAD','TAX',
-        'PTRATIO','B','LSTAT',
-        'MEDV']
-    assert df.columns.to_list() == boston_cols
+# def test_load_boston():
+#     df = data.load_boston()
+#     boston_shape = (506, 14)
+#     assert df.shape == boston_shape
+#     boston_cols = [
+#         'CRIM','ZN','INDUS',
+#         'CHAS','NOX','RM',
+#         'AGE','DIS','RAD','TAX',
+#         'PTRATIO','B','LSTAT',
+#         'MEDV']
+#     assert df.columns.to_list() == boston_cols
 

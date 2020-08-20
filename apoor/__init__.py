81d7480b13bb826e700c15f3fafb31c628321902
a-poor <a-poor@users.noreply.github.com>
2020-08-19 22:34:25 -0400

    Removing bad import_ fn

--- apoor/__init__.py
+++ apoor/__init__.py
@@ -6,33 +6,6 @@ created by Austin Poor
 __version__ = "1.0.0"
 
 
-def import_(verbose=True):
-    """
-    imports numpy, pandas, pyplot,
-    and seaborn (and calls sns.set())
-    and adds them to the global namespace.
-
-    if `verbose` is True, prints the import
-    statements.
-    """
-    global np
-    global pd
-    global plt
-    global sns
-    if verbose:
-        print(
-        "import numpy as np\n"
-        "import pandas as pd\n"
-        "import matplotlib.pyplot as plt\n"
-        "import seaborn as sns\n"
-        "sns.set()"
-        )
-    import numpy as np
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    sns.set()
-
 def make_scale(dmin,dmax,rmin,rmax,clamp=False):
     """
     Creates a scale function to map a number from
--- tests/test_apoor.py
+++ tests/test_apoor.py
@@ -1,13 +1,6 @@
 
 import apoor
 
-def test_import_():
-    apoor.import_(False)
-    assert "np"  in dir()
-    assert "pd"  in dir()
-    assert "plt" in dir()
-    assert "sns" in dir()
-
 def test_make_scale():
     assert apoor.make_scale(0,1,0,10)(0.1) == 1
     assert apoor.make_scale(10,0,0,1)(9) == 0.1

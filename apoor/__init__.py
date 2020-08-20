7c81138cfaaf0fafcece4ab2bbf4c4ac1b755562
a-poor <a-poor@users.noreply.github.com>
2020-08-19 20:54:54 -0400

    Adding bumpversion, setup.py, and function docstrings

--- apoor/__init__.py
+++ apoor/__init__.py
@@ -3,11 +3,30 @@ apoor.__init__.py
 created by Austin Poor
 """
 
-def import_():
+__version__ = "0.0.1"
+
+
+def import_(verbose=True):
+    """
+    imports numpy, pandas, pyplot,
+    and seaborn (and calls sns.set())
+    and adds them to the global namespace.
+
+    if `verbose` is True, prints the import
+    statements.
+    """
     global np
     global pd
     global plt
     global sns
+    if verbose:
+        print(
+        "import numpy as np\n"
+        "import pandas as pd\n"
+        "import matplotlib.pyplot as plt\n"
+        "import seaborn as sns\n"
+        "sns.set()"
+        )
     import numpy as np
     import pandas as pd
     import matplotlib.pyplot as plt
@@ -15,6 +34,23 @@ def import_():
     sns.set()
 
 def make_scale(dmin,dmax,rmin,rmax,clamp=False):
+    """
+    Creates a scale function to map a number from
+    a domain to a range.
+
+    Inputs:
+        dmin, dmax: numeric start and end values
+            for the domain
+        rmin, rmax: numeric start and end values
+            for the range
+        clamp: if the value is outside the range,
+            return clamped value (default: False)
+    Returns:
+        scale: function taking one numeric argument
+            and returns the value mapped from the
+            domain to the range (and clamped if
+            `clamp` flag is set).
+    """
     drange = dmax - dmin
     rrange = rmax - rmin
     scale_factor = rrange / drange
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="apoor",
+    version="0.0.1",
+    author="Austin Poor",
+    author_email="a-poor@users.noreply.github.com",
+    description="A small personal package written in Python with reusable code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/a-poor/apoor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning"
+    ],
+    python_requires=">=3.6"
+)
